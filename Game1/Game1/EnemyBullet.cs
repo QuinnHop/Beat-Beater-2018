56fdbe3ccab3ed146b05bdb9347ef222d0563f55
Quinn Hopwood <qph6412@rit.edu>
2018-02-23 10:20:15 -0500

    Added classes for player, basic parent class and enemy bullets

---
+++ Game1/Game1/EnemyBullet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game1
+{
+    class EnemyBullet
+    {
+        //fields
+
+
+        //properties
+
+        //constructor
+
+        //methods
+
+    }
+}
---
+++ Game1/Game1/GameObject.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game1
+{
+    class GameObject
+    {
+        //fields
+        protected Rectangle position;
+        protected Texture2D texture;
+
+        //properties
+        public int PositionX
+        {
+            get
+            {
+                return position.X;
+            }
+
+            set
+            {
+                position.X = value;
+            }
+        }
+
+        public int PositionY
+        {
+            get
+            {
+                return position.Y;
+            }
+
+            set
+            {
+                position.Y = value;
+            }
+        }
+
+        //constructor
+
+        public GameObject(int x, int y, int height, int width)
+        {
+            position = new Rectangle(x, y, width, height);
+        }
+        //methods
+
+
+
+
+    }
+}
